3cbe09f81bdbb1aeae86198782d97db5c5cccb80
Thomas Breitburd <tmb76@cam.ac.uk>
2024-06-11 15:46:56 +0100

    Added code to plot the results

--- src/mod_3_q_1.py
+++ src/mod_3_q_1.py
@@ -7,6 +7,8 @@ objective function
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 # ---------------------------------------------
 # Define some functions
@@ -75,10 +77,17 @@ eta = 1 / L
 x_star = x_init.copy()
 i = 0
 
+x1 = [x_init[0]]
+x2 = [x_init[1]]
+
 # Perform the gradient descent
 while epsilon > 1e-2:
     # Update the current value
     x_star = x_star - eta * grad_f(x_star)
+
+    # Append the current value
+    x1.append(x_star[0])
+    x2.append(x_star[1])
 
     # Calculate the new error
     epsilon = f(x_star) - f(x_obj)
@@ -94,3 +103,32 @@ print("---------------------------------")
 print(f"Final value = {x_star}")
 print(f"Final difference = {epsilon}")
 print(f"Number of iterations = {i}")
+
+
+# ---------------------------------------------
+# Plot some results
+# ---------------------------------------------
+
+plt.style.use("seaborn-v0_8-darkgrid")
+
+plt.figure(figsize=(4, 4))
+
+plt.plot(x1, "o-", label="x1")
+plt.plot(x2, "x-", label="x2")
+
+plt.xlabel("Iteration")
+plt.ylabel("Value")
+plt.legend()
+plt.title("Convergence of the gradient descent")
+
+plt.tight_layout()
+
+# Save the plot
+cur_dir = os.getcwd()
+plots_dir = os.path.join(cur_dir, "Plots")
+os.makedirs(plots_dir, exist_ok=True)
+
+plot_dir = os.path.join(plots_dir, "q3_1_Convergence.png")
+plt.savefig(plot_dir)
+
+plt.close()
