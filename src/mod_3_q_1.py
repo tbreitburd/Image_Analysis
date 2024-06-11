7082c958bad7679e1bc96df44df393fb2672ee7f
Thomas Breitburd <tmb76@cam.ac.uk>
2024-06-11 15:39:12 +0100

    Added code for gradient descent
    Defined the parameter/x values and wrote code for the gradient descent.

--- src/mod_3_q_1.py
+++ src/mod_3_q_1.py
@@ -8,7 +8,12 @@ objective function
 
 import numpy as np
 
+# ---------------------------------------------
+# Define some functions
+# ---------------------------------------------
 
+
+# Define the objective function from the paper
 def f(x):
     """!@brief Objective function
 
@@ -20,6 +25,8 @@ def f(x):
     return (1 / 2) * x1**2 + x2**2
 
 
+# Define the gradient of that objective function
+# for gradient descent
 def grad_f(x):
     """!@brief Gradient of the objective function
 
@@ -31,6 +38,59 @@ def grad_f(x):
     return np.array([x1, 2 * x2])
 
 
-x_init = np.array([1, 1])
+# Define the epsilon function, the error between the current value and the objective value
+def eps(x):
+    """!@brief Epsilon, the error between the current value and the objective value
 
+    @param x: The input vector [x1, x2]
+    @return The value of epsilon
+    """
+
+    return np.linalg.norm(x - x_obj)
+
+
+# ---------------------------------------------
+# Define some parameters
+# ---------------------------------------------
+
+# Define the initial and objective values
+x_init = np.array([1, 1])
 x_obj = np.array([0, 0])
+
+# Calculate the initial difference
+epsilon = f(x_init) - f(x_obj)
+print("---------------------------------")
+print(f"Initial difference = {epsilon}")
+
+# Define the learning rate
+L = 2
+eta = 1 / L
+
+
+# ---------------------------------------------
+# Perform the gradient descent
+# ---------------------------------------------
+
+# Initialize the current value
+x_star = x_init.copy()
+i = 0
+
+# Perform the gradient descent
+while epsilon > 1e-2:
+    # Update the current value
+    x_star = x_star - eta * grad_f(x_star)
+
+    # Calculate the new error
+    epsilon = f(x_star) - f(x_obj)
+
+    # Count the iteration
+    i += 1
+
+
+# ---------------------------------------------
+# Print the results
+# ---------------------------------------------
+print("---------------------------------")
+print(f"Final value = {x_star}")
+print(f"Final difference = {epsilon}")
+print(f"Number of iterations = {i}")
