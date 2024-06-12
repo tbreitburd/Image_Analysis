eac5c531f5c63f6044950658cd89799d5902597d
Thomas Breitburd <tmb76@cam.ac.uk>
2024-06-12 15:17:59 +0100

    Added code for the mod_1_tulips file
    used chan vese segmentation, after switching to HSV format, thresholding, and gaussian blurring the image

---
+++ src/mod_1_tulips.py
@@ -0,0 +1,79 @@
+"""!@file mod_1_tulips.py
+
+@brief This file contains code for the segmentation of the tulips image
+
+@detail Uses...
+
+@author T.Breitburd on 09/06/2024"""
+
+
+import skimage
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.filters import threshold_otsu
+from skimage.morphology import opening, disk
+from skimage.segmentation import chan_vese
+import os
+
+# Set the random seed
+np.random.seed(75016)
+
+# Load the image
+tulips = skimage.io.imread("./data/noisy_flower.jpg")
+tulips = tulips[:, :, :3]
+
+# ----------------------------------------
+# Preprocess the image
+# ----------------------------------------
+
+# Switch to Hue-Saturation-Value (HSV) color space
+tulips_hsv = skimage.color.rgb2hsv(tulips)
+
+# ----------------------------------------
+# Segmentation
+# ----------------------------------------
+
+# Threshold the image on hue channel
+thresh = threshold_otsu(tulips_hsv[:, :, 0])
+tulips_mask = tulips_hsv[:, :, 0] > thresh
+
+# Apply opening to remove the small white spots, and keep the larger ones (purple tulips)
+op_mask = opening(tulips_mask)
+
+# Gaussian blur to smooth the mask
+blurred_mask = skimage.filters.gaussian(op_mask, sigma=2)
+
+# Apply the Chan-Vese segmentation, get
+cv_mask = chan_vese(blurred_mask, mu=0.1, lambda2=1.5)
+
+# Apply opening
+cv_mask_op = opening(cv_mask, disk(4))
+
+# ----------------------------------------
+# Plot the results
+# ----------------------------------------
+
+fig, ax = plt.subplots(2, 2, figsize=(10, 10))
+
+ax[0, 0].imshow(tulips)
+ax[0, 0].set_title("Original Image")
+
+ax[0, 1].imshow(blurred_mask, cmap="gray")
+ax[0, 1].set_title("Blurred Binary Image")
+
+ax[1, 0].imshow(cv_mask, cmap="gray")
+ax[1, 0].set_title("Chan-Vese Segmentation")
+
+ax[1, 1].imshow(cv_mask_op, cmap="gray")
+ax[1, 1].set_title("Opened Final Image")
+
+plt.tight_layout()
+# Save the plot
+cur_dir = os.getcwd()
+plots_dir = os.path.join(cur_dir, "Plots")
+os.makedirs(plots_dir, exist_ok=True)
+
+plot_dir = os.path.join(plots_dir, "tulip_segmentation.png")
+plt.savefig(plot_dir)
+
+plt.close()
